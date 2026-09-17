4a1465f6270ab81d302259ddc3a7fd3a66c020d8
solarygold0-coder <solarygold0@gmail.com>
2026-09-17 13:14:01 +0300

    Load print styles for patient and appointment views

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 import './rtl.css';
+import './print.css';
 
 document.documentElement.lang = 'ar';
 document.documentElement.dir = 'rtl';
