e7a513b1ede914155cf50537312e3193aed40ff3
solarygold0-coder <solarygold0@gmail.com>
2026-09-17 14:31:30 +0300

    Load hardened RTL UI overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 import './rtl.css';
+import './ui-fixes.css';
 import './print.css';
 
 document.documentElement.lang = 'ar';
